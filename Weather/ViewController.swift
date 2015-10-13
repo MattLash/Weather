944550d3da6481ffb40d593708594df21884dd03
Matt Lashinsky <malashinsky@gmail.com>
2015-10-13 16:18:40 -0400

    Initial Files to Commit
    Committing these files for first time.

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,10 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cityEntered = String()
+    var validCity = String()
+    
+    @IBOutlet var cityField: UITextField!
+    
+   
+    @IBAction func buttonClick(sender: AnyObject) {
+        
+        if cityField != nil {
+            
+            cityEntered = String(cityField.text!)
+            
+            let cityArray = cityEntered.componentsSeparatedByString(" ")
+            
+            if cityArray.count > 1 {
+                
+                for x in cityArray {
+                    
+                    validCity += x
+                }
+            }
+            
+            print(validCity)
+        }
+        
+        let cityString = "http://www.weather-forecast.com/locations/" + validCity.lowercaseString + "/forecasts/latest"
+        
+        let url = NSURL(string: cityString)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (dataCopy , response, error ) -> Void in
+            //code here:
+            if dataCopy != nil {
+                
+                let webContent = NSString(data: dataCopy!, encoding: NSUTF8StringEncoding)
+                
+//                print(webContent)
+                
+                let webContentString = NSString(string: webContent!)
 
+                if webContentString.containsString("1 – 3 Day Weather"){
+                    print("seems to be finding the 1-3 Day summary")
+                } else {
+                    print("not finding the 1-3 day summary, HOE!")
+                }
+                
+            } else {
+                print("dataCopy variable is empty")
+            }
+        }
+        
+        print(cityString)
+        
+        task.resume()
+    }
+
+    @IBOutlet var textField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
